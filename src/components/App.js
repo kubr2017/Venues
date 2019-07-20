462d77a825499233eb8e368ac3b9077adca063c1
r <kubr2017@gmail.com>
2019-07-20 10:51:41 -0400

    Feat: First steps in hierarhy.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../App.css';
+import SearchLocation from './SearchLocation';
+
+class App extends Component {
+
+  render(){
+    return (
+      <div className="App">
+        App
+        <SearchLocation/>
+      </div>
+    );
+  }
+};
+
+export default App;
---
+++ src/components/SearchLocation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SearchLocation extends Component {
+
+  render(){
+    return(
+      <div>
+        SearchLocation
+      </div>
+    )
+  }
+};
+
+export default SearchLocation;
