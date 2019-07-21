cbd2ab7cbf178bcf7b8ad03b6edd08c604b44fe1
r <kubr2017@gmail.com>
2019-07-21 10:45:26 -0400

    Feat: wire up thunk.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
 
+
+const store = createStore(reducers,applyMiddleware(thunk));
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={ store }>
     <App />
   </Provider>, document.getElementById('root'));
 
