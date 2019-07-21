166450665e8de1eff444765c8a6351330ddd59f7
r <kubr2017@gmail.com>
2019-07-20 20:58:02 -0400

    Feat: Applied css styles to components for comfort.

--- src/components/App.js
+++ src/components/App.js
@@ -11,12 +11,20 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        App
-        <SearchLocation/>
-        <Miles/>
-        <Location/>
-        <List/>
-        <Map/>
+        <div className='App-title-box'>
+          App
+        </div>
+        <div className='App-venues-box'>
+          <div className='App-search-location-box'>
+            <SearchLocation/>
+            <Miles/>
+          </div>
+          <List/>
+        </div>
+        <div className='App-map-box'>
+          <Location/>
+          <Map/>
+        </div>
       </div>);
   }
 };
