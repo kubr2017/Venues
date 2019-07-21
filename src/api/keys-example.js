1f73f1a5675bc80c0594e931d49b1f8a17224b9a
r <kubr2017@gmail.com>
2019-07-21 10:26:35 -0400

    Feat: Finish key-example.js file.

---
+++ src/api/keys-example.js
@@ -0,0 +1,7 @@
+//Put your own API keys in bottom and rename file to key.js
+
+import React from 'react'
+
+export const FourSquareClient_id='GX************0G'
+export const FourSquareClient_secret='PR************SW'
+export const GoogleAPIkey='KH************************Tz'
