425808ee2ccb87e84066b35427da26360248f8d8
r <kubr2017@gmail.com>
2019-08-01 13:44:08 -0400

    Feat: search problem

--- src/components/Map.js
+++ src/components/Map.js
@@ -9,8 +9,19 @@ class Map extends Component {
 
   componentDidMount(){
     this.props.getGoogle();
+    window.initMap = () => {
+      var map = new window.google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: {lat:40.6947591, lng:-73.9950086},
+        mapTypeControl: false
+      })
+      // ???????????????????????? Why impossible this.map  and => var window.map  ?
+      window.map = map
+      //console.log('inside initMap window.map:',window.map);
+      //console.log('<Map/> google:',window.google);
+    }
   }
-
+  
   render(){
     console.log('From STATE googleObject:',this.props.googleObject);
     return (
