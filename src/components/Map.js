67202f137af94811565ed4f5cab2721e4bdf87bd
r <kubr2017@gmail.com>
2019-08-01 14:08:01 -0400

    Feat: Got map from state and reneder it eventually.

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,29 +2,51 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { GoogleAPIkey } from '../api/keys';
 import { getGoogleMaps } from '../api/GoogleConnection';
-import { initMap } from '../api/GoogleConnection';
+// import { initMap } from '../api/GoogleConnection';
 import { getGoogle } from '../actions/index';
 
 class Map extends Component {
 
+  initMap(){
+    console.log('inside initMap window.map:',window.map);
+    console.log('<Map/> google:',window.google);
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: {lat:40.6947591, lng:-73.9950086},
+      mapTypeControl: false
+    })
+    console.log();
+    window.map = map
+  }
+
+
   componentDidMount(){
+    console.log('inside DidMount()');
     this.props.getGoogle();
-    window.initMap = () => {
-      var map = new window.google.maps.Map(document.getElementById('map'), {
+  }
+
+  renderMap = (location) => {
+    console.log('inside initMap window.map:',window.map);
+    console.log('inside if google:',window.google);
+    if(window.google){
+      console.log('inside if window.map:',window.map);
+      console.log('inside if google:',window.google);
+      var map = new this.props.googleObject.Map(document.getElementById('map'), {
         zoom: 15,
-        center: {lat:40.6947591, lng:-73.9950086},
+        center: location,
         mapTypeControl: false
       })
-      // ???????????????????????? Why impossible this.map  and => var window.map  ?
-      window.map = map
-      //console.log('inside initMap window.map:',window.map);
-      //console.log('<Map/> google:',window.google);
+      console.log();
+      window.map = map;
     }
+
   }
-  
+
   render(){
     console.log('From STATE googleObject:',this.props.googleObject);
+    this.renderMap({lat:40.6947591, lng:-73.9950086});
     return (
+
       <div className="Map">
         Map
         <div id="map">map</div>
