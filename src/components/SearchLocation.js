b7346bda35b39fa371e5827fe3a9f114ebbdb73e
r <kubr2017@gmail.com>
2019-08-04 13:35:52 -0400

    Feat: First approach.

--- src/components/SearchLocation.js
+++ src/components/SearchLocation.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class SearchLocation extends Component {
 
-  
+  renderAutocomplete(){
+    console.log('in renderAutocomplete, condition:',(Object.entries(this.props.googleObject).length === 0 && this.props.googleObject.constructor === Object));
+    if(!(Object.entries(this.props.googleObject).length === 0 && this.props.googleObject.constructor === Object)){
+      let googleObject = this.props.googleObject;
+      console.log('in renderAutocomplete, in IF statement, googleObject:',googleObject);
+      let input = document.getElementById('searchInput');
+      var autoComplete = new googleObject.places.Autocomplete(input);
+    }
+  }
 
   render(){
+    console.log('in SearchLocation, in render');
+    this.renderAutocomplete();
     return(
       <div className='SearchLocation'>
         SearchLocation
-        <div><input id='searchInput' /></div>
+        <div><input id='searchInput'/></div>
       </div>
     )
   }
 };
 
-export default SearchLocation;
+const mapStateToProps = state => {
+  return { googleObject:state.googleObject }
+}
+
+export default connect(mapStateToProps)(SearchLocation);
