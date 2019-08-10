9d87ee336b3d5dfb314c550092f362c590cae881
r <kubr2017@gmail.com>
2019-08-09 22:52:55 -0400

    Feat: 1 step closer.

--- src/components/Venues.js
+++ src/components/Venues.js
@@ -6,29 +6,57 @@ import { getVenuesDetails } from '../actions';
 
 class Venues extends Component {
 
-  renderVenues(){
+  renderVenues() {
     console.log('in renderVenues');
-
-    // this.props.getVenues();
-    // const Id='4aad3536f964a520035f20e3'
-    //
-    // this.props.getDetails(Id);
-
-    //
-    if(this.props.location){
+    if(this.props.location) {
       console.log('lat:',this.props.location.lat(),' lng:',this.props.location.lng());
-
+      console.log('in <Venues/> props:',this.props);
+      console.log('venuesDEtails parse:',this.props.venuesDetails.map((venue)=>(venue.data.response.venue)));
+      // return this.props.venuesDetails.map((venue)=>(venue.data.response.venue).map((venue)=>(returnCard()));
     }
+  }
 
-    console.log('in <Venues/> props:',this.props);
+
+
+  renderCard(){
+    return(
+      <div className='Venues-card'>
+        <div className = 'Venues-card-head'>
+          <div className = 'Venues-card-head-name'>
+          name
+          </div>
+          <div className = 'Venues-card-head-rate'>
+          rate
+          </div>
+        </div>
+        <div className = 'Venues-card-details'>
+          <div className = 'Venues-card-details-pic'>
+          pic
+          </div>
+          <div className = 'Venues-card-details-menu'>
+          menu
+          </div>
+          <div className = 'Venues-card-details-time'>
+          time
+          </div>
+          <div className = 'Venues-card-details-address'>
+          address
+          </div>
+        </div>
+      </div>
+    )
 
   }
 
+
   render(){
-    this.renderVenues();
+
+
     return (
       <div className="Venues">
         Venues
+        {this.renderCard()}
+        {this.renderVenues()}
       </div>
     );
   }
